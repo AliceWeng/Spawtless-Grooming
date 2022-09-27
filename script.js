f652bb8bc011cfccc58f2c7b7a61b0167809000b
Alice <alicewengs@gmail.com>
2022-09-26 23:04:51 -0700

    added a gallery for dog pictures

---
+++ script.js
@@ -0,0 +1,8 @@
+function left(){
+    let left = document.querySelector(".dog-pics");
+    left.scrollBy(-500, 0);
+}
+function right(){
+    let right = document.querySelector(".dog-pics");
+    right.scrollBy(500, 0);
+}
